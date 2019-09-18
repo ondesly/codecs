f571b335073c4bb04fa4d433211aded976310945
ondesly <dmitriytorkhov@gmail.com>
2019-09-18 09:44:31 +0300

    initial

---
+++ src/codecs.cpp
@@ -0,0 +1,20 @@
+//
+// Created by ondesly on 2019-09-18.
+// Copyright (c) 2019 ondesly. All rights reserved.
+//
+
+#include <base64/libbase64.h>
+#include <msgpack/msgpack.hpp>
+#include <openssl/aes.h>
+#include <openssl/crypto.h>
+#include <zlib.h>
+
+#include "codecs.h"
+
+// Zip
+
+// Aes
+
+// Base64
+
+// Msg
---
+++ src/codecs.h
@@ -0,0 +1,49 @@
+//
+// Created by ondesly on 2019-09-18.
+// Copyright (c) 2019 ondesly. All rights reserved.
+//
+
+#pragma once
+
+#include <string>
+#include <unordered_map>
+
+namespace codecs {
+
+    class zip {
+    public:
+
+        static std::string encode(const std::string &data);
+
+        static std::string decode(const std::string &data);
+
+    };
+
+    class aes {
+    public:
+
+        static std::string encode(const char *key, const char *iv, const std::string &data);
+
+        static std::string decode(const char *key, const char *iv, const std::string &data);
+
+    };
+
+    class base64 {
+    public:
+
+        static std::string encode(const std::string &data);
+
+        static std::string decode(const std::string &data);
+
+    };
+
+    class msg {
+    public:
+
+        static std::string encode(const std::unordered_map<int, std::string> &data);
+
+        static std::unordered_map<int, std::string> decode(const std::string &data);
+
+    };
+
+}
